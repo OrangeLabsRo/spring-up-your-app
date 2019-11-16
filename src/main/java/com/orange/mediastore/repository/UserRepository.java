1ea75947b6737fe91bffcf9ad7dc0b029105810a
Dan.Petrescu <dan.petrescu@orange.com>
2019-11-16 10:01:28 +0100

    SecurityStep1

---
+++ src/main/java/com/orange/mediastore/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.orange.mediastore.repository;
+
+import com.orange.mediastore.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    User findByUsername(String username);
+
+}
---
+++ src/main/java/com/orange/mediastore/service/UserService.java
@@ -0,0 +1,23 @@
+package com.orange.mediastore.service;
+
+import com.orange.mediastore.model.User;
+import com.orange.mediastore.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+    }
+}
