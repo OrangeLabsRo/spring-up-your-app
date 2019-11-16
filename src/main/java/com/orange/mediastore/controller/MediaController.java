519e0c5c2315d93de3e20b5c0f3d50188d7f5d82
Dan.Petrescu <dan.petrescu@orange.com>
2019-11-16 06:52:04 +0100

    small changes in order to work

--- src/main/java/com/orange/mediastore/controller/MediaController.java
+++ src/main/java/com/orange/mediastore/controller/MediaController.java
@@ -4,12 +4,16 @@ import com.orange.mediastore.model.Media;
 import com.orange.mediastore.service.MediaService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static com.orange.mediastore.configuration.MediaConstants.API_V1;
+
 @RestController
+@RequestMapping(API_V1)
 public class MediaController {
     private MediaService service;
 
